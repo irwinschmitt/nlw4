e76f93637192fe4d888198fe195921d5173ddf6e
Irwin Schmitt <irwinschmitt@gmail.com>
2021-03-03 06:51:09 -0300

    Migrate experience bar from CSS to Material-UI

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,6 +1,31 @@
+import {
+  Box,
+  createStyles,
+  LinearProgress,
+  Theme,
+  ThemeProvider,
+  Typography,
+  withStyles,
+} from "@material-ui/core";
 import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
-import styles from "../styles/components/ExperienceBar.module.css";
+
+const ExperienceLinearProgress = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: 10,
+      borderRadius: 5,
+    },
+    colorPrimary: {
+      backgroundColor:
+        theme.palette.grey[theme.palette.type === "light" ? 200 : 700],
+    },
+    bar: {
+      borderRadius: 5,
+      backgroundColor: "#4cd62b",
+    },
+  })
+)(LinearProgress);
 
 export function ExperienceBar() {
   const { currentExperience, experienceToNextLevel } = useContext(
@@ -11,18 +36,23 @@ export function ExperienceBar() {
     Math.round(currentExperience * 100) / experienceToNextLevel;
 
   return (
-    <header className={styles.experienceBar}>
-      <span>0 xp</span>
-      <div>
-        <div style={{ width: `${percentToNextLevel}%` }} />
-        <span
-          className={styles.currentExperience}
-          style={{ left: `${percentToNextLevel}%` }}
-        >
-          {currentExperience} xp
-        </span>
-      </div>
-      <span>{experienceToNextLevel} xp</span>
+    <header>
+      <Box display="flex" alignItems="center">
+        <Box minWidth={24}>
+          <Typography>0xp</Typography>
+        </Box>
+
+        <Box width="100%" mx={2}>
+          <ExperienceLinearProgress
+            variant="determinate"
+            value={percentToNextLevel}
+          />
+        </Box>
+
+        <Box minWidth={45}>
+          <Typography>{experienceToNextLevel}xp</Typography>
+        </Box>
+      </Box>
     </header>
   );
 }
